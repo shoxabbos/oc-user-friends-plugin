b3cb9eca739dae74f506f39739f4dbd3fe44fe36
Shohabbos <19941001@inbox.ru>
2019-09-01 17:13:34 +0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -19,6 +19,24 @@ class Plugin extends PluginBase
 
         User::extend(function(User $model) {
             $model->hasMany['friends'] = \Shohabbos\Friends\Models\Friend::class;
+            $model->addDynamicMethod('getFriendStatus', function ($user_id) use ($model) {
+                foreach ($model->friends as $friend) {
+                    // $arr = [
+                    //     1 => 'request_sent',
+                    //     2 => 'request_got',
+                    //     3 => 'denied',
+                    //     4 => 'was_denied',
+                    //     5 => 'friends' 
+                    // ];
+                    if ($friend->friend_id == $user_id && $friend->status)
+                    {
+                        return $friend->status;
+                    } elseif ($friend->friend_id == $user_id)
+                    {
+                        return false;
+                    }
+                }
+            });
         });
 
     }
@@ -35,7 +53,6 @@ class Plugin extends PluginBase
     // 
     // Helpers
     //
-
     private function extendUserMenu($manager) {
         $manager->addSideMenuItems('RainLab.User', 'user', [
             'friends' => [
